c10c2d6dac1b3fc0301b32d332451e2661124a36
Benedict Osei Sefa <benedictoseisefa@Benedicts-MacBook-Pro.local>
2022-07-11 13:34:34 +0000

    second commit

--- berlApp/urls.py
+++ berlApp/urls.py
@@ -1,5 +1,5 @@
-from django import path
+from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.detailedUser, name='detailedUser'),
+    path('', views.detaileduser, name='detaileduser'),
 ]
--- berlApp/views.py
+++ berlApp/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from .models import *
 # Create your views here.
-def detailedUser(request):
+def detaileduser(request):
     if request.method == 'POST':
-        fullname    = request.POST['fullname']
+        fullName    = request.POST['fullname']
         email       = request.POST['email']
         studentId   = request.POST['studentId']
         gender      = request.POST['gender']
         roomtype    = request.POST['roomtype']
         room_choosing_status = request.POST['room_choosing_status']
         bio         = request.POST['bio']
-        detaileduser = DetailedUser.objects.create(fullname=fullname, email=email, studentId=studentId, gender=gender, roomtype=roomtype, room_choosing_status=room_choosing_status, bio=bio)
+        detaileduser = DetailedUser.objects.create(fullName=fullName, email=email, studentId=studentId, gender=gender, roomtype=roomtype, room_choosing_status=room_choosing_status, bio=bio)
         if DetailedUser.objects.filter(roomtype='FOUR IN A ROOM SELF-CONTAIN WITH BALCONY KICHENNETTE'):
             MRoom1mw.name.add(detaileduser)
     return render(request, 'detaileduser.html')
